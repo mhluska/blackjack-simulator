3bf2f24e4f779d5af93943ad312074057dec5f0d
Maros Hluska <mhluska@gmail.com>
2021-09-03 21:56:58 -0700

    Prevent removing listener on invalid action

--- src/browser/player-input-reader.ts
+++ src/browser/player-input-reader.ts
@@ -3,16 +3,15 @@ import { actionDataKeyToCorrectMove, Move } from '../types';
 
 export default class DOMPlayerInputReader implements PlayerInputReader {
   readInput(callback: (action: Move) => void): void {
-    document.body.addEventListener(
-      'keypress',
-      (event) => {
-        const action = actionDataKeyToCorrectMove(event.key);
-        if (action) {
-          callback(action);
-        }
-      },
-      { once: true }
-    );
+    const keypressHandler = (event: KeyboardEvent) => {
+      const action = actionDataKeyToCorrectMove(event.key);
+      if (action) {
+        document.body.removeEventListener('click', clickHandler);
+        callback(action);
+      }
+    };
+
+    document.body.addEventListener('keypress', keypressHandler);
 
     const clickHandler = (event: Event) => {
       if (!(event.target instanceof HTMLElement)) {
