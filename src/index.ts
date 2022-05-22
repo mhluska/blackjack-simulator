95e27d2fcc6b7e0806fdea90f55576998c2f3b74
Maros Hluska <mhluska@gmail.com>
2022-05-22 16:39:18 -0700

    Export game settings

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-import Game from './game';
+import Game, { settings } from './game';
 import { Event } from './event-emitter';
 import Simulator from './simulator';
 import PlayerInputReader from './browser/player-input-reader';
 import { GameStep, GameMode } from './types';
 
-export { Game, Event, Simulator, PlayerInputReader, GameStep, GameMode };
+export {
+  Game,
+  Event,
+  Simulator,
+  PlayerInputReader,
+  GameStep,
+  GameMode,
+  settings as gameSettings,
+};
